27e807cd1cf723583bb8734cc315dbe6c5fd452d
megkthrock <megkthrock@gmail.com>
2016-02-25 20:48:47 -0500

    Test passing number of students in each pair

--- CreateRandomPairs.rb
+++ CreateRandomPairs.rb
@@ -1,5 +1,19 @@
 def create_random_pairs(array_of_students)
-	array = [array_of_students]
+	
+	if array_of_students.count <= 2
+		array = [array_of_students]
+	else
+		student_1 = array_of_students.sample
+		array_of_students.delete(student_1)
+		student_2 = array_of_students.sample
+		array_of_students.delete(student_2)
+		
+		first_pair = [student_1,student_2]
+	
+		total_pairs = [first_pair,array_of_students]
+
+	end
+
 end
 
 
--- CreateRandomPairsTest.rb
+++ CreateRandomPairsTest.rb
@@ -10,7 +10,14 @@ class Test_Random_Pairs <Minitest::Test
 	def test_with_two_students_get_single_pair
 		array_of_pairs = create_random_pairs(["Jennifer", "Shane"])
 		assert_equal(1, array_of_pairs.count)
+		assert_equal(2, array_of_pairs[0].count)
 	end
 
+	def test_with_four_students_get_two_pairs
+		array_of_pairs = create_random_pairs(["Jennifer","Shane","Meg","Dolly"])
+		assert_equal(2, array_of_pairs.count)
+		assert_equal(2, array_of_pairs[0].count)
+		assert_equal(2, array_of_pairs[1].count)
+	end
 
 end
